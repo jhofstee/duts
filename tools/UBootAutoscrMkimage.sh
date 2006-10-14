226d936cbe01aef82baaf2b10288411bfb1875b8
Rafal Jaworowski <raj@semihalf.com>
2006-10-14 23:07:38 +0200

    Improve and reorganize UBoot script image test case.

--- tools/UBootAutoscrMkimage.sh
+++ tools/UBootAutoscrMkimage.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
 # Creates an example UBoot script image. Output is suitable for DULG.
-# $1: board name (used to get the location to store the image)
+# $1: filename of UBoot source script
+# $2: filename of the resultant image
 #
 
 echo 'bash$ mkimage -A ppc -O linux -T script -C none -a 0 -e 0 \'
 echo '> -n "autoscr example script" \'
-echo "> -d example.script /tftpboot/$1/example.img"
+echo "> -d $1 $2"
 
 mkimage -A ppc -O linux -T script -C none -a 0 -e 0 \
 -n "autoscr example script" \
--d example.script /tftpboot/$1/example.img
+-d $1 $2
